11b4813c09440811e48333a3d834ff116c4d196d
Sean <shonkelli@gmail.com>
2020-04-24 02:00:23 -0400

    added prompt to enter developer name

--- js/generateDocumentation.js
+++ js/generateDocumentation.js
@@ -1,39 +1,60 @@
-// generateDocumentation
-const ruleName = document.querySelector(
-  ".TitleText---page_header"
-).innerText
+// generateDocumentation.js
 
-let ticket = window.prompt(
-  "Ticket number for this object",
-  // default response
-  "NO TICKET"
-);
+chrome.storage.local.get('developerName', function (result) {
 
-const formatDate = () => {
-  let today = new Date()
-  let dd  = (today.getDate() < 10 ? '0' : '') + today.getDate();
-  let month = ((today.getMonth() + 1) < 10 ? '0' : '') + (today.getMonth() + 1);
-  let yyyy = today.getFullYear()
-  return (yyyy + "-" + month + "-" + dd);
-}
+  let developerName
 
-let date = formatDate()
-console.log("This is working")
-document.activeElement.value=
-`/*
+  if (result.developerName){
+    developerName = result.developerName
+  } else {
+    developerName = window.prompt(
+      "Enter your name",
+      "No Name"
+    )
+    // Save the name
+    chrome.storage.local.set({developerName: developerName}, function() {
+      console.log('Developer name set to ' + developerName);
+    })
+  }
+
+  // generateDocumentation
+  let ruleName = document.querySelector(
+    ".TitleText---page_header"
+  ).innerText
+
+  let ticket = window.prompt(
+    "Ticket number for this object",
+    // default response
+    "NO TICKET"
+  );
+
+  let formatDate = () => {
+    let today = new Date()
+    let dd  = (today.getDate() < 10 ? '0' : '') + today.getDate();
+    let month = ((today.getMonth() + 1) < 10 ? '0' : '') + (today.getMonth() + 1);
+    let yyyy = today.getFullYear()
+    return (yyyy + "-" + month + "-" + dd);
+  }
+
+  let date = formatDate()
+
+  document.activeElement.value=
+  `/*
   ${ruleName}(
 
   )
-*/
+  */
 
 
 
 
-/*
-////////////////////////////// CHANGE LOG \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  /*
+  ////////////////////////////// CHANGE LOG \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-<${date}><${ticket}><${MY_NAME}>
+  <${date}><${ticket}><${developerName}>
   --Created
 
-*/
-`
+  */
+  `
+
+})
