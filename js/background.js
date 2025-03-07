2ab12ab13a05fbd87fdbcba116eafef004ae2919
unknown <shonkelli@gmail.com>
2025-03-06 21:20:47 -0500

    Refactor Insert Debug Text Field context menu action to be event-driven service worker and compatible with manifest V3

--- js/background.js
+++ js/background.js
@@ -1,73 +1,68 @@
-// contextMenus
-const contextMenus = {};
-
 // Create Rule Comments
-  chrome.runtime.onInstalled.addListener( () => {
-    chrome.contextMenus.create({
-      id: "generateRuleComments",
-      title: "Generate Rule Comments",
-      contexts: ["editable", "page"],
+chrome.runtime.onInstalled.addListener( () => {
+  chrome.contextMenus.create({
+    id: "generateRuleComments",
+    title: "Generate Rule Comments",
+    contexts: ["editable", "page"],
+    documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*"]
+  });
+  if(chrome.runtime.lastError){
+    console.log(chrome.runtime.lastError.message)
+  }
+});
+
+// Create Changelog Entry
+chrome.runtime.onInstalled.addListener( () => {
+  chrome.contextMenus.create({
+      id: "generateChangelogEntry",
+      title: "Generate Changelog Entry",
+      contexts: ["editable","page"],
       documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*"]
     });
     if(chrome.runtime.lastError){
       console.log(chrome.runtime.lastError.message)
     }
-  });
+});
 
-  // Create Changelog Entry
-  chrome.runtime.onInstalled.addListener( () => {
-    chrome.contextMenus.create({
-        id: "generateChangelogEntry",
-        title: "Generate Changelog Entry",
-        contexts: ["editable","page"],
-        documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*"]
-      });
-      if(chrome.runtime.lastError){
-        console.log(chrome.runtime.lastError.message)
-      }
+// Replace Native Components with Wrappers
+chrome.runtime.onInstalled.addListener( () => {
+  chrome.contextMenus.create({
+    id: "replaceNativeComponentsWithWrappers",
+    title: "Replace Native Components with Wrappers",
+    contexts: ["selection"],
+    documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*"]
   });
+  if(chrome.runtime.lastError){
+    console.log(chrome.runtime.lastError.message)
+  }
+});
 
-  // Replace Native Components with Wrappers
-  chrome.runtime.onInstalled.addListener( () => {
-    chrome.contextMenus.create({
-      id: "replaceNativeComponentsWithWrappers",
-      title: "Replace Native Components with Wrappers",
-      contexts: ["selection"],
-      documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*"]
+// Convert to/from index function
+chrome.runtime.onInstalled.addListener( () => {
+  chrome.contextMenus.create({
+    id: "convertToOrFromIndexFunction",
+    title: "Convert to/from index function",
+    contexts: ["selection"],
+    documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*"]
+  });
+  if(chrome.runtime.lastError){
+    console.log(chrome.runtime.lastError.message)
+  }
+});
+
+
+// Insert Debug Text Field Component
+chrome.runtime.onInstalled.addListener( () => {
+  chrome.contextMenus.create({
+      id: "insertDebugTextField",
+      title: "Insert Debug Text Field Component",
+      contexts: ["editable","page"],
+      documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*", "https://*.appscloud.fisheries.noaa.gov/suite/design/*" ]
     });
     if(chrome.runtime.lastError){
       console.log(chrome.runtime.lastError.message)
     }
-  });
-
-    // Convert to/from index function
-    chrome.runtime.onInstalled.addListener( () => {
-      chrome.contextMenus.create({
-        id: "convertToOrFromIndexFunction",
-        title: "Convert to/from index function",
-        contexts: ["selection"],
-        documentUrlPatterns: ["https://*.appiancloud.us/suite/design/*"]
-      });
-      if(chrome.runtime.lastError){
-        console.log(chrome.runtime.lastError.message)
-      }
-    });
-
-
-    // Insert Debug Text Field Component
-    contextMenus.insertDebugTextField =
-      chrome.contextMenus.create(
-        {
-          "title": "Insert Debug Text Field Component",
-          "contexts": ["editable","page"],
-          "documentUrlPatterns": ["https://*.appiancloud.us/suite/design/*"]
-        },
-        function(){
-          if(chrome.runtime.lastError){
-            console.log(chrome.runtime.lastError.message)
-          }
-        }
-      )
+});
 
 
 
@@ -106,10 +101,13 @@ function contextMenuHandler(info, tab){
         console.error("Error executing script:", error)
       });
     break;
-    case contextMenus.insertDebugTextField:
-      chrome.tabs.executeScript({
-        file: 'js/insertDebugTextField.js'
-      })
+    case "insertDebugTextField":
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ['js/insertDebugTextField.js']
+      }).catch((error) => {
+        console.error("Error executing script:", error)
+      });
       break;
 
     // default:
@@ -121,22 +119,22 @@ chrome.contextMenus.onClicked.addListener(contextMenuHandler)
 
 // Open new tab and show rfr
 chrome.runtime.onMessage.addListener(function (message){
-    if (message.type === "openRFREditor") {
-      chrome.tabs.create(
-        { url: chrome.runtime.getURL("rfr_editor.html") },
-        function ( tab ) {
-          var handler = function(tabId, changeInfo) {
-            if(tabId === tab.id && changeInfo.status === "complete"){
-              chrome.tabs.onUpdated.removeListener(handler);
-              chrome.tabs.sendMessage(tabId, {rfrId: message.rfrId});
-            }
+  if (message.type === "openRFREditor") {
+    chrome.tabs.create(
+      { url: chrome.runtime.getURL("rfr_editor.html") },
+      function ( tab ) {
+        var handler = function(tabId, changeInfo) {
+          if(tabId === tab.id && changeInfo.status === "complete"){
+            chrome.tabs.onUpdated.removeListener(handler);
+            chrome.tabs.sendMessage(tabId, {rfrId: message.rfrId});
           }
-          // in case we're faster than page load (usually):
-        chrome.tabs.onUpdated.addListener(handler);
-        // just in case we're too late with the listener:
-        chrome.tabs.sendMessage(tab.id, {rfrId: message.rfrId});
         }
-      );
-    }
+        // in case we're faster than page load (usually):
+      chrome.tabs.onUpdated.addListener(handler);
+      // just in case we're too late with the listener:
+      chrome.tabs.sendMessage(tab.id, {rfrId: message.rfrId});
+      }
+    );
   }
+}
 )
